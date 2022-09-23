bc1ed8fa4bd1c89f956d1d4e0edccbaa050beba2
it794613 <it794613@naver.com>
2022-09-23 17:34:53 +0900

    Initial Commit

---
+++ todolist2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolist2
+//
+//  Created by 최진용 on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
